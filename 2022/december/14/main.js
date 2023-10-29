77239ef009c4ffe4b9e03b9f82fea63ab393cf58
Kevin Wilson <kevin9092@hotmail.co.za>
2023-10-29 22:39:52 +0000

    14 December 2022

---
+++ 2022/december/14/main.js
@@ -0,0 +1,19 @@
+// Add JavaScript code here
+const menu = {};
+
+menu.init = () => {
+  const controls = document.querySelectorAll(".menu__content button");
+  controls.forEach((control) => {
+    control.addEventListener("click", (event) => menu.submit(event));
+  });
+};
+
+menu.submit = (event) => {
+  event.target.setAttribute("data-animate", "start");
+  event.target.setAttribute("data-animate", "bounce");
+  setTimeout(() => {
+    event.target.setAttribute("data-animate", "complete");
+  }, 150);
+};
+
+menu.init();
